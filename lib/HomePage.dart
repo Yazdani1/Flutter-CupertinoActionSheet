dbc0f06cdbdaf9b9df23798c0eefb9288a258704
Yazdani <yaz4dani@gmail.com>
2019-04-12 12:35:09 +0600

    Initial commit

---
+++ lib/HomePage.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => new _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+
+      appBar: new AppBar(
+        title: new Text("Capertino Widget"),
+        backgroundColor: Colors.indigo,
+      ),
+
+      body: new Container(
+        child: new Center(
+
+            child: new CupertinoButton(
+              child: new Text("Click here"),
+              onPressed: (){
+                shoCuportionDialog(context);
+              },
+              color: Colors.redAccent,
+
+            ),
+
+        ),
+      ),
+
+    );
+  }
+  
+  shoCuportionDialog(BuildContext context){
+
+    showCupertinoModalPopup(
+        context: context,
+        builder: (context){
+
+          return CupertinoActionSheet(
+            title: new Text("First time learning"),
+            message: new Text("Cupertion learning things"),
+            cancelButton: new CupertinoActionSheetAction(
+                onPressed: (){
+                  Navigator.of(context).pop();
+                },
+                child: new Text("Cancel")
+            ),
+            actions: <Widget>[
+
+              new CupertinoActionSheetAction(
+                  onPressed: (){},
+                  child: new Text("First Page",
+                  style: TextStyle(fontSize: 22.0,color: Colors.orange),
+                  ),
+              ),
+              
+              new CupertinoActionSheetAction(
+                  onPressed: (){}, 
+                  child: new Text("Second Page")
+              ),
+              
+
+            ],
+          );
+
+        }
+    );
+
+
+  }
+  
+}
---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:capertino_model/HomePage.dart';
+
+
+void main(){
+  runApp(new MaterialApp(
+    home: new Home(),
+  ));
+}
