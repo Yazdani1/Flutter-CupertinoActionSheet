528fbb7a9c5424bef4612c8d61308c5f4e45f791
Yazdani <yaz4dani@gmail.com>
2019-04-14 12:55:01 +0600

    second commit

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -36,7 +36,7 @@ class _HomeState extends State<Home> {
   }
   
   shoCuportionDialog(BuildContext context){
-
+    
     showCupertinoModalPopup(
         context: context,
         builder: (context){
